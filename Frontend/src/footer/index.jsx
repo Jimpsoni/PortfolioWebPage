52015929cdf381ff7b590e246e896559d648bb94
Jimi Jukkala <99808538+Jimpsoni@users.noreply.github.com>
2023-11-21 02:45:22 +0200

    Added the draft of Footer

---
+++ Frontend/src/footer/index.jsx
@@ -0,0 +1,26 @@
+import './styles.css'
+
+const Footer = () => {
+  return (
+    <footer id='mainFooter'>
+      <address id='addressess'>
+        <ul id='addresslist'>
+          <a href='google.com'>
+            <li>Github</li>
+          </a>
+          <a href='google.com'>
+            <li>LinkedIn</li>
+          </a>
+          <a href='google.com'>
+            <li>Instagram</li>
+          </a>
+          <a href='google.com'>
+            <li>Jukkalajimi@gmail.com</li>
+          </a>
+        </ul>
+      </address>
+    </footer>
+  )
+}
+
+export default Footer
