65e2e4a5b7becf3a851e017e5f8ba1e6c842476a
Jimi Jukkala <99808538+Jimpsoni@users.noreply.github.com>
2023-11-23 01:42:55 +0200

    Add files via upload

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,11 +1,56 @@
-import { useEffect } from 'react'
 import './App.css'
+import { useEffect } from 'react'
 import NavBar from './components/navbar'
 import MainContent from './components/maincontent'
 import Footer from './components/footer'
 import ContactMe from './components/contactme'
 
 function App() {
+  useEffect(() => {
+    function random(min, max) {
+      return min + Math.random() * (max + 1 - min)
+    }
+
+    const elem = document.getElementById('aboutme')
+    const canvasSize = elem.offsetWidth * elem.offsetHeight
+    const starsFraction = canvasSize / 20000
+
+    for (let i = 0; i < starsFraction; i++) {
+      // Set up random elements
+      let xPos = random(0, 100)
+      let yPos = random(0, 100)
+      let alpha = random(0.5, 1)
+      let size = random(1, 2)
+      let colour = '#E8E8E8'
+
+      const star = document.createElement('div')
+      star.style.position = 'absolute'
+      star.style.left = xPos + '%'
+      star.style.top = yPos + '%'
+      star.style.opacity = alpha
+      star.style.width = size + 'px'
+      star.style.height = size + 'px'
+      star.style.backgroundColor = colour
+      star.style.zIndex = 2
+      document.getElementById('aboutme').appendChild(star)
+    }
+  }, [])
+
+
+  return (
+    <>
+      <NavBar />
+      <ContactMe />
+      <MainContent />
+      <Footer />
+    </>
+  )
+}
+
+export default App
+
+
+/*
   useEffect(() => {
     function random(min, max) {
       return min + Math.random() * (max + 1 - min)
@@ -36,14 +81,4 @@ function App() {
     }
   }, [])
 
-  return (
-    <>
-      <NavBar />
-      <ContactMe />
-      <MainContent />
-      <Footer />
-    </>
-  )
-}
-
-export default App
+*/
