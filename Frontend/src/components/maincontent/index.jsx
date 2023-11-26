bebfdf64712fa450620d612c9c9b145d8ea8d269
Jimi Jukkala <99808538+Jimpsoni@users.noreply.github.com>
2023-11-26 18:51:35 +0200

    updated layout

--- Frontend/src/components/maincontent/index.jsx
+++ Frontend/src/components/maincontent/index.jsx
@@ -33,34 +33,42 @@ const MainContent = () => {
               I&rsquo;m a passionate and creative full-stack developer with a
               focus on React and JavaScript
             </p>
-            <h3 className='skillHeader'>Python</h3>
-            <p className='descriptionText'>
-              I&rsquo;ve wrote most of my lines using Python. Tons of small
-              personal projects and bigger projects for excercises left me with
-              a lot of knowledge. Online courses I have taken:
-            </p>
 
-            <ul id='certifications'>
-              <li className='certification'>Data engineering With Python</li>
-              <li className='certification'>Data-analysis with Python</li>
-              <li className='certification'>Scientific Computing with Python</li>
-            </ul>
+            <div className='skill'>
+              <h3 className='skillHeader'>Python</h3>
+              <p className='descriptionText'>
+                I&rsquo;ve wrote most of my lines using Python. Tons of small
+                personal projects and bigger projects for excercises left me
+                with a lot of knowledge. Online courses I have taken:
+              </p>
 
-            <h3 className='skillHeader'>HTML, CSS, JS</h3>
-            <p className='descriptionText'>
-              I&rsquo;ve wrote most of my lines using Python. Tons of small
-              personal projects and bigger projects for excercises left me with
-              a lot of knowledge
-            </p>
+              <ul id='certifications'>
+                <li className='certification'>Data engineering With Python</li>
+                <li className='certification'>Data-analysis with Python</li>
+                <li className='certification'>
+                  Scientific Computing with Python
+                </li>
+              </ul>
+            </div>
 
-            <h3 className='skillHeader'>Databases</h3>
-            <p className='descriptionText'>
-              I&rsquo;ve wrote most of my lines using Python. Tons of small
-              personal projects and bigger projects for excercises left me with
-              a lot of knowledge
-            </p>
+            <div className='skill'>
+              <h3 className='skillHeader'>HTML, CSS, JS</h3>
+              <p className='descriptionText'>
+                I&rsquo;ve wrote most of my lines using Python. Tons of small
+                personal projects and bigger projects for excercises left me
+                with a lot of knowledge
+              </p>
+            </div>
+
+            <div className='skill'>
+              <h3 className='skillHeader'>Databases</h3>
+              <p className='descriptionText'>
+                I&rsquo;ve wrote most of my lines using Python. Tons of small
+                personal projects and bigger projects for excercises left me
+                with a lot of knowledge
+              </p>
+            </div>
           </div>
-
 
           <div className='skillSection'>
             <h2 className='subheading2'>Practical skills</h2>
@@ -68,43 +76,45 @@ const MainContent = () => {
               I&rsquo;ve gained lot of valuable skills through my hobbies and
               work life
             </p>
-            <h3 className='skillHeader'>Teamwork</h3>
-            <p className='descriptionText'>
-              I&rsquo;ve wrote most of my lines using Python. Tons of small
-              personal projects and bigger projects for excercises left me with
-              a lot of knowledge
-            </p>
 
-            <h3 className='skillHeader'>Communications</h3>
-            <p className='descriptionText'>
-              I&rsquo;ve wrote most of my lines using Python. Tons of small
-              personal projects and bigger projects for excercises left me with
-              a lot of knowledge
-            </p>
+            <div className='skill'>
+              <h3 className='skillHeader'>Teamwork</h3>
+              <p className='descriptionText'>
+                I&rsquo;ve wrote most of my lines using Python. Tons of small
+                personal projects and bigger projects for excercises left me
+                with a lot of knowledge
+              </p>
+            </div>
 
-            <h3 className='skillHeader'>Good work morale</h3>
-            <p className='descriptionText'>
-              I&rsquo;ve wrote most of my lines using Python. Tons of small
-              personal projects and bigger projects for excercises left me with
-              a lot of knowledge
-            </p>
+            <div className='skill'>
+              <h3 className='skillHeader'>Communications</h3>
+              <p className='descriptionText'>
+                I&rsquo;ve wrote most of my lines using Python. Tons of small
+                personal projects and bigger projects for excercises left me
+                with a lot of knowledge
+              </p>
+            </div>
+
+            <div className='skill'>
+              <h3 className='skillHeader'>Good work morale</h3>
+              <p className='descriptionText'>
+                I&rsquo;ve wrote most of my lines using Python. Tons of small
+                personal projects and bigger projects for excercises left me
+                with a lot of knowledge
+              </p>
+            </div>
           </div>
         </section>
       </div>
 
+      <div className='spacer2 layer2'></div>
+      <div id='jobexperience'>
       <h2 className='subheading'>Job experience</h2>
       <p className='sectionText'>
         I am self-employed and currently work with Jyväsmuutot, providing a
         range of moving and transportation services on a freelance basis.
       </p>
-
-      <h2 className='subheading'>My Skills and projects</h2>
-      <p className='sectionText'>
-        I&rsquo;m a passionate and creative full-stack developer with a focus on
-        React and JavaScript. At 22, I&rsquo;ve already immersed myself in
-        diverse projects, gaining hands-on experience in creating scalable and
-        interactive web applications.
-      </p>
+      </div>
     </>
   )
 }
